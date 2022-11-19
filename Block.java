cc63e5204547ccee98549fdccfd5b11e0242bc60
voquangduyqti <117366154+voquangduyqti@users.noreply.github.com>
2022-11-19 20:17:51 +0700

    Nhập số khối bằng tay
    Windows PowerShell
    Copyright (C) Microsoft Corporation. All rights reserved.
    
    Try the new cross-platform PowerShell https://aka.ms/pscore6
    
    PS C:\Users\Admin\Documents\GitHub\xhcnm>  & 'C:\Program Files\Java\jdk-19\bin\java.exe' '--enable-preview' '-XX:+ShowCodeDetailsInExceptionMessages' '@C:\Users\Admin\AppData\Local\Temp\cp_2qkn609dl3z43iglkt2qao13u.argfile' 'VQD_Chain'
    H?y nh?p s? l??ng kh?i c?a chu?i: 
    3
    Kh?i t?o thông tin kh?i ??u tiên!
    02345aqwe
    Trying to Mine block 0... 
    Block Mined!!! : 0000087f1bcf618b78cbeb12ba3bc20e771f77764e8de6cc1c037d4f9becc7c3
    Nh?p d? li?u c?a kh?i th?: 1
    Nh?p h? và tên:
    Võ Quang Duy
    Nh?p m? h?p ??ng: 
    012356789
    Trying to Mine block 1
    Block Mined!!! : 000000507074618be53aba85fc336411f36e2722173d53e9a8329009e5af6759
    Nh?p d? li?u c?a kh?i th?: 2
    Nh?p h? và tên:
    Nguyễn Quang Vũ
    Nh?p m? h?p ??ng: 
    1234567890
    Trying to Mine block 2
    Block Mined!!! : 000006d222cf0d93cbb948fca9e750d9facc6a3f3d5e9ceb7d8f52d1c2a9eb7d
    
    Blockchain is Valid: true
    
    The block chain:
    [
      {
        "hash": "0000087f1bcf618b78cbeb12ba3bc20e771f77764e8de6cc1c037d4f9becc7c3",
        "previousHash": "0",
        "data": "02345aqwe",
        "timeStamp": 0,
        "nonce": 1282639
      },
      {
        "hash": "000000507074618be53aba85fc336411f36e2722173d53e9a8329009e5af6759",
        "previousHash": "0000087f1bcf618b78cbeb12ba3bc20e771f77764e8de6cc1c037d4f9becc7c3",
        "data": "Vo Quang Duy-012356789",
        "timeStamp": 0,
        "nonce": 814987
      },
      {
        "hash": "000006d222cf0d93cbb948fca9e750d9facc6a3f3d5e9ceb7d8f52d1c2a9eb7d",
        "previousHash": "000000507074618be53aba85fc336411f36e2722173d53e9a8329009e5af6759",
        "data": "Nguy?n Quang Vu-1234567890",
        "timeStamp": 0,
        "nonce": 1056680
      }
    ]

---
+++ Block.java
@@ -0,0 +1,35 @@
+import java.sql.Date;
+public class Block {
+public String hash;
+public String previousHash;
+private String data;
+private long timeStamp;
+private int nonce;
+//Phương thức khởi tạo khối.
+public Block(String data,String previousHash ) {
+this.data = data;
+this.previousHash = previousHash;
+this.timeStamp = new Date(timeStamp).getTime();
+this.hash = calculateHash();
+// Phương thức tạo hash và đào khối sẽ được viết ở đây
+    }
+public String calculateHash() {
+    String calculatedhash = DigitalSignatures.applySha256(
+    previousHash +
+    Long.toString(timeStamp) +
+    Integer.toString(nonce) +
+    data
+    );
+    return calculatedhash;
+    }
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0'
+        , '0');
+        //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+        nonce ++;
+        hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+        } 
+}
